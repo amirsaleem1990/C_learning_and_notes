ebe93338b7f70d1b552711d79f4631ebd71e5598
home <amirsaleem1990@hotmail.com>
2018-12-19 14:00:26 +0500

    updated

--- p1.c
+++ p1.c
@@ -1,29 +1,4 @@
 #include <stdio.h> 
-struct book
-{
-	int bookid;
-	char title[20];
-	float price;
-};
-struct book input()
-{
-	struct book b;
-	printf("Enter book id, title and price\n");
-	scanf("%d", &b.bookid);
-	fflush(stdin);
-	scanf("%s%f", &b.title, &b.price);
-	return(b);
-}
-void display(struct book bb)
-{
-	printf("\nid: %d \ntitle:%s \nprice:%f\n", bb.bookid, bb.title, bb.price);
-}
-void main()
-{
-	struct book b1;
-	b1 = input();
-	display(b1);
-}
 
 
 
